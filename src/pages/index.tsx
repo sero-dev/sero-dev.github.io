c232e04251f62ee9ce07d951a3ab16cfadf3ed51
Sean Rodriguez <sean.rodriguez@outlook.com>
2020-12-07 23:22:37 -0500

    Rename IndexPage file to .tsx; Remove boilerplate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+
+function IndexPage() {
+
+  const name: string = 'Sean'
+
+  return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+  )
+}
+
+export default IndexPage
