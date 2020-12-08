f5c2971cf8be59fb7dfdee23a7af877c3bcaa08b
Sean Rodriguez <sean.rodriguez@outlook.com>
2020-12-07 22:40:26 -0500

    Remove sitemap plugin until it can be implemented properly

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,6 @@ module.exports = {
     "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
