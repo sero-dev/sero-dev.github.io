7b86386e4da9608d936319e981e96afca2880a2d
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-01-05 20:11:45 -0500

    Add all sections for landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
-import * as React from "react"
+import * as React from 'react'
+import Layout from '../components/shared/Layout';
+import Navigation from '../components/shared/Navigation';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Experiences from '../components/Experiences';
+import Projects from '../components/Projects';
+import Contact from '../components/Contact';
+import Footer from '../components/shared/Footer';
 
-function IndexPage() {
-
-  const name: string = 'Sean'
-
+const IndexPage: React.FC = () => {
   return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
+    <Layout>
+      <Navigation />
+      <Hero />
+      <About />
+      <Experiences />
+      {/*<Projects />*/}
+      {/*<Contact />*/}
+      <Footer />
+    </Layout>
   )
 }
 
