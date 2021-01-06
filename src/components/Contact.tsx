a798d92115d39a3470327bb5790e28b81015fd46
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-01-05 20:11:18 -0500

    Add contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Contact: React.FC = () => {
+
+  return (
+    <section id="contact">
+      <h2>Get In Touch</h2>
+    </section>
+  )
+}
+
+export default Contact;
