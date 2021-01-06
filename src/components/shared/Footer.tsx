cf6ea59335da9fc3b3051dac7d575f39b1675bb6
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-01-05 20:09:51 -0500

    Add footer component

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import {footerContainer} from '../../styles/Footer.module.css'
+
+const Footer: React.FC = () => {
+
+  return (
+    <footer className={footerContainer}>
+      <p>Design and Built by Sean Rodriguez</p>
+    </footer>
+  )
+}
+
+export default Footer;
