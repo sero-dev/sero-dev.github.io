b3d74a3c67792a7b505597c52e2f87982b07c0e4
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-01-05 20:11:57 -0500

    Add project component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const Projects: React.FC = () => {
+
+  return (
+    <section id="projects">
+      <h2>What I've Built</h2>
+    </section>
+  )
+}
+
+export default Projects;
