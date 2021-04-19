3fe806ce1b41cb6702384b36897c6c5b8f457b65
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-04-18 20:02:47 -0400

    Remove CSS Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,10 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-postcss",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-transformer-typescript-css-modules",
     "gatsby-plugin-fontawesome-css",
     {
       resolve: "gatsby-source-filesystem",
